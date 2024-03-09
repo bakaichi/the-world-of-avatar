1c247cd23f70ee1cc4e2139a3026b4d42e9ccd13
Aidas Vaiciunas <vaiciunasaidas@gmail.com>
2024-03-09 23:34:57 +0000

    Joi validate for user signup

--- src/controllers/accounts-controller.js
+++ src/controllers/accounts-controller.js
@@ -1,5 +1,6 @@
 /* eslint-disable quotes */
 import { db } from "../models/db.js";
+import { UserSpec } from "../models/joi-schemas.js";
 
 export const accountsController = {
   index: {
@@ -18,6 +19,13 @@ export const accountsController = {
 
   signup: {
     auth: false,
+    validate: {
+      payload: UserSpec,
+      options: { abortEarly: false },
+      failAction: function(request, h, error) {
+        return h.view("signup-view", { title: "Sign up error", errors: error.details }).takeover().code(400);
+      },
+    },
     handler: async function (request, h) {
       const user = request.payload;
       await db.userStore.addUser(user);
